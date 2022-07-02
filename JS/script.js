9145846bff0f3761267183ba532e1aa5833df4ee
Oleg Danilov <oleg-danilov8@ya.ru>
2022-07-02 13:59:17 +0300

    rewriting JQuery to JS

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,30 @@
-$(document).ready(function () {
-    $('.header__burger').click(function() {
-        $('.header__burger,.header__menu').toggleClass('active');
-    });
-
-    // Фильтр изображений
-    $('.boost__swapItem').click(function() {
-        let i = $(this).data('filter');
-        $('.boost__backImg').hide();
-        $('.boost__backImg.f_' + i).show();
+// $(document).ready(function () {
+//     // Фильтр изображений
+//     $('.boost__swapItem').click(function() {
+//         let i = $(this).data('filter');
+//         $('.boost__backImg').fadeOut(500);
+//         $('.boost__backImg.f_' + i).fadeIn(500);
         
-        $('.boost__swapItem').removeClass('active-swap');
-        $(this).addClass('active-swap');
-        return false;
-    })
-})
+//         $('.boost__swapItem').removeClass('active-swap');
+//         $(this).addClass('active-swap');
+//     })
+// })
+
+let swapButton = document.querySelectorAll('.boost__swapItem');
+swapButton.forEach(button => {
+    button.onclick = changeImg;
+});
+
+function changeImg() {
+    let imgNum = this.dataset.filter;
+    
+}
+
+document.querySelector('.header__burger').onclick = () => {
+    document.querySelector('.header__burger').classList.toggle('active');
+    document.querySelector('.header__menu').classList.toggle('active');
+}
+
 
 // Плавный скролл
 document.querySelectorAll('a[href^="#"').forEach(link => {
