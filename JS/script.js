b3b347b9a4b953012c8c039428884f3de3b52b75
Oleg Danilov <oleg-danilov8@ya.ru>
2022-07-09 00:05:06 +0300

    add smoothScroll func

--- JS/script.js
+++ JS/script.js
@@ -17,19 +17,15 @@ function changeImg() {
 
 // Плавный скролл
 document.querySelectorAll('a[href^="#"]').forEach(link => {
-
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        let href = this.getAttribute('href').substring(1);
-        const scrollTarget = document.getElementById(href);
-
-        const elementPosition = scrollTarget.getBoundingClientRect().top;
-        const offsetPosition = elementPosition - topOffset;
-
-        window.scrollBy({
-            top: offsetPosition,
-            behavior: 'smooth'
-        });
-    });
+    link.addEventListener('click', smoothScroll);
 });
+
+function smoothScroll(e) {
+    e.preventDefault();
+    let href = this.getAttribute('href').substring(1);
+    const scrollTarget = document.getElementById(href);
+    window.scrollBy({
+        top: scrollTarget.getBoundingClientRect().top,
+        behavior: 'smooth'
+    });
+}
