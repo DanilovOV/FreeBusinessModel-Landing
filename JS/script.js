1795ee5d04f68b938eea97a430da97429bab8d4a
Oleg Danilov <oleg-danilov8@ya.ru>
2022-07-08 23:59:57 +0300

    delete offset

--- JS/script.js
+++ JS/script.js
@@ -22,11 +22,7 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
         e.preventDefault();
 
         let href = this.getAttribute('href').substring(1);
-
         const scrollTarget = document.getElementById(href);
-
-        // Высота фиксированного заголовка
-        const topOffset = 0;
 
         const elementPosition = scrollTarget.getBoundingClientRect().top;
         const offsetPosition = elementPosition - topOffset;
